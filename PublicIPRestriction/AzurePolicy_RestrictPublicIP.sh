061df1cd7de79dedcaa12cd4a6fd33d963c7f3ac
Florent Appointaire <florent.appointaire@gmail.com>
2018-12-19 09:36:39 +0100

    Delete AzurePolicy_RestrictPublicIP.sh

--- PublicIPRestriction/AzurePolicy_RestrictPublicIP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-az account set --subscription ${1}
-path=${2}
-rulesPath="$path/PublicIPRestriction/azurepolicy.rules.json"
-az policy definition create --name 'restrict-public-ip' --display-name 'Restrict Public IP' \
-    --description 'Restrict Public IP resource from being associated to a NIC' \
-    --rules $rulesPath --mode All
-
-az policy assignment create --name 'Restrict Public IP' --scope * --policy "restrict-public-ip" 
